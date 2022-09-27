73d3a96c0cdc6dd07d81b1ec1f4f1c9b58a45ed4
unknown <asif88383@gmail.com>
2022-09-27 14:21:41 +0500

    Defaultoverlay component added

--- src/components/DefaultOverlayContent/index.tsx
+++ src/components/DefaultOverlayContent/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Container, Heading, Buttons } from "./styles";
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
+interface Props {
+  label: string;
+  description: string;
 }
 
-export default index
+const DefaultOverlayContent: React.FC<Props> = ({ label, description }) => {
+  return (
+    <Container>
+      <Heading>
+        <h1>{label}</h1>
+        <h2>{description}</h2>
+      </Heading>
+
+      <Buttons>
+        <button>Custom Order</button>
+        <button className="white">Existing Inventory</button>
+      </Buttons>
+    </Container>
+  );
+};
+
+export default DefaultOverlayContent;
